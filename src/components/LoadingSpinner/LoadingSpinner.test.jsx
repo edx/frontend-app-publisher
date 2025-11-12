cd71bcc5a618f3745b855c3e604e1e88a2aad615
smaheshkumarreddy-sonata <smaheshkumarreddy-sonata@2u.com>
2025-11-12 11:09:33 +0000

    This is for fork testing-1

--- src/components/LoadingSpinner/LoadingSpinner.test.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 
 import LoadingSpinner from './index';
-
+// Thi for fork related testing purpose only before adding protection rules
 describe('LoadingSpinner', () => {
   it('shows a loading spinner', async () => {
     const { container } = render(<LoadingSpinner />);
