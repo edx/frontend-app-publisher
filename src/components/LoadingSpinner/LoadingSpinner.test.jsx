2ef36bbf2fc7894b1b9ce4cd6a94c96a24d93a27
smaheshkumarreddy-sonata <smaheshkumarreddy-sonata@2u.com>
2025-11-13 06:50:34 +0000

    test after adding protection rules

--- src/components/LoadingSpinner/LoadingSpinner.test.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.test.jsx
@@ -3,6 +3,7 @@ import { render, waitFor } from '@testing-library/react';
 
 import LoadingSpinner from './index';
 //This is for fork related testing purpose only before adding protection rules(test-ulmo)
+//This is for fork related testing purpose only before adding protection rules(test-ulmo)
 describe('LoadingSpinner', () => {
   it('shows a loading spinner', async () => {
     const { container } = render(<LoadingSpinner />);
